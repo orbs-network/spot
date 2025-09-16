d7bd14ed3f9d004eea59419eb308f77bebb8f835
DanielZlotin <danielzlotin@users.noreply.github.com>
2025-09-17 00:50:18 +0300

    script

--- script/04_DeployExecutor.s.sol
+++ script/04_DeployExecutor.s.sol
@@ -9,9 +9,9 @@ import {Executor} from "src/executor/Executor.sol";
 contract DeployExecutor is BaseScript {
     function run() public returns (address executor) {
         address reactor = vm.envAddress("REACTOR");
-        address admin = vm.envAddress("ADMIN");
+        address wm = vm.envAddress("WM");
 
-        bytes32 hash = hashInitCode(type(Executor).creationCode, abi.encode(reactor, admin));
+        bytes32 hash = hashInitCode(type(Executor).creationCode, abi.encode(reactor, wm));
         console.logBytes32(hash);
 
         bytes32 salt = vm.envOr("SALT", bytes32(0));
@@ -19,7 +19,7 @@ contract DeployExecutor is BaseScript {
 
         if (executor.code.length == 0) {
             vm.broadcast();
-            Executor deployed = new Executor{salt: salt}(reactor, admin);
+            Executor deployed = new Executor{salt: salt}(reactor, wm);
             require(executor == address(deployed), "executor mismatched address");
         } else {
             console.log("executor already deployed");
