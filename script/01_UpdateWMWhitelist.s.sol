118e4923a59574548034875f759c672171f9b5ae
DanielZlotin <danielzlotin@users.noreply.github.com>
2025-09-17 01:12:38 +0300

    scripts

--- script/01_UpdateWMWhitelist.s.sol
+++ script/01_UpdateWMWhitelist.s.sol
@@ -9,6 +9,9 @@ import {WM} from "src/WM.sol";
 
 contract UpdateWMWhitelist is BaseScript {
     uint256 public constant BATCH_SIZE = 300;
+    uint256 public constant MAX_WHITELIST_SIZE = 200;
+    string internal constant WM_LIST_FILE = "wm.json";
+    string internal constant WM_EXTRA_LIST_FILE = "wm2.json";
 
     function run() public {
         WM admin = WM(payable(vm.envAddress("WM")));
@@ -18,9 +21,31 @@ contract UpdateWMWhitelist is BaseScript {
             return;
         }
 
-        address[] memory list = abi.decode(
-            vm.parseJson(vm.readFile(string.concat(vm.projectRoot(), "/script/input/", "whitelist.json"))), (address[])
+        string memory root = string.concat(vm.projectRoot(), "/script/input/");
+
+        address[] memory mainList = abi.decode(
+            vm.parseJson(vm.readFile(string.concat(root, WM_LIST_FILE))), (address[])
         );
+
+        uint256 limit = mainList.length < MAX_WHITELIST_SIZE ? mainList.length : MAX_WHITELIST_SIZE;
+
+        require(limit > 0, "whitelist empty");
+
+        address[] memory incrementalList = abi.decode(
+            vm.parseJson(vm.readFile(string.concat(root, WM_EXTRA_LIST_FILE))), (address[])
+        );
+
+        uint256 combinedLength = limit + incrementalList.length;
+
+        address[] memory list = new address[](combinedLength);
+
+        for (uint256 i = 0; i < limit; i++) {
+            list[i] = mainList[i];
+        }
+
+        for (uint256 i = 0; i < incrementalList.length; i++) {
+            list[limit + i] = incrementalList[i];
+        }
 
         if (admin.allowed(list[0]) && admin.allowed(list[list.length - 1])) {
             console.log("wm whitelist already updated");
